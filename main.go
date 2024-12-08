2158f8872b4617f1276f35d910dd5e4da087eaff
Tokenzrey <msyarifhh@gmail.com>
2024-12-08 23:20:34 +0700

    fix: origin cors

--- main.go
+++ main.go
@@ -22,14 +22,14 @@ func main() {
     // Inisialisasi router
     r := gin.Default()
 
-    // Middleware CORS
+    // Middleware CORS: Mengizinkan semua origin
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:3000"}, // Domain yang diizinkan
-        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Metode yang diizinkan
-        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Header yang diizinkan
-        ExposeHeaders:    []string{"Content-Length"}, // Header yang dapat diakses di client
-        AllowCredentials: true, // Mengizinkan kredensial (cookies, Authorization header, dll.)
-        MaxAge:           12 * time.Hour, // Durasi cache untuk preflight request
+        AllowOrigins:     []string{"*"}, 
+        AllowMethods:     []string{"*"}, 
+        AllowHeaders:     []string{"*"},
+        ExposeHeaders:    []string{"*"}, 
+        AllowCredentials: false, //
+        MaxAge:           12 * time.Hour,
     }))
 
     // Register routes
