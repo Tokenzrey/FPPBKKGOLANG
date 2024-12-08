1fe971fc4b643c4b9cda909bebafb00132a5206b
Tokenzrey <msyarifhh@gmail.com>
2024-12-08 21:53:49 +0700

    feat: add cors

--- main.go
+++ main.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Tokenzrey/FPPBKKGOLANG/api/router"
 	"github.com/Tokenzrey/FPPBKKGOLANG/config"
 	"github.com/Tokenzrey/FPPBKKGOLANG/db/initializers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
-	config.LoadEnvVariables()
-	initializers.ConnectDB()
+    config.LoadEnvVariables()
+    initializers.ConnectDB()
 }
 
 func main() {
-	fmt.Println("BE Berhasil!")
-	r := gin.Default()
-	router.GetRoute(r)
+    fmt.Println("BE Berhasil!")
 
-	r.Run()
+    // Inisialisasi router
+    r := gin.Default()
+
+    // Middleware CORS
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:3000"}, // Domain yang diizinkan
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Metode yang diizinkan
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Header yang diizinkan
+        ExposeHeaders:    []string{"Content-Length"}, // Header yang dapat diakses di client
+        AllowCredentials: true, // Mengizinkan kredensial (cookies, Authorization header, dll.)
+        MaxAge:           12 * time.Hour, // Durasi cache untuk preflight request
+    }))
+
+    // Register routes
+    router.GetRoute(r)
+
+    // Jalankan server
+    r.Run()
 }
